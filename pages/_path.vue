a0a935ba72212ebb499d5de5176e8d138aa1ee97
Greg Arutunyan <gregartist@gmail.com>
2020-12-14 09:25:18 -0800

    testing azure static web apps auto deploy

---
+++ pages/_path.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ this.path }}</h1>
+</template>
+
+<script>
+export default {
+    async asyncData({ params }) {
+        return new Promise((resolve, reject) => {
+            resolve({
+                path: params.path
+            })
+        })
+    }
+}
+</script>
